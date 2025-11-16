61da29090c6ad3b112550738f8cf02021100fc0f
atallahomar <omaratallah2970@gmail.com>
2025-11-16 15:48:31 +0100

    6th push

--- ConferenceApp/urls.py
+++ ConferenceApp/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import *
 urlpatterns =[
- #path("liste/", views.list_conferences, name="liste_conferences"),
     path("liste/",ConferenceList.as_view(),name="liste_conferences"),
     path("<int:pk>/",ConferenceDetails.as_view(),name="conference_details"),
     path("add/",ConferenceCreate.as_view(),name="conference_add"),
